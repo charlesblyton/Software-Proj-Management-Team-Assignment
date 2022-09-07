42399bccc4bdc92b9b02106826604ebfc4432c76
jeremygilbertc <jeremy.gilbert.bm@gmail.com>
2022-09-07 20:34:41 +1000

    Update TestClassJeremy.java

--- TestClassJeremy.java
+++ TestClassJeremy.java
@@ -2,5 +2,6 @@ public class TestClassJeremy {
     public static void main(String[] args) {
         System.out.println("Is this working?");
         System.out.println("Yes I can see your file Jeremy");
+        System.out.println("Thanks for the validation!")
     }
 }
