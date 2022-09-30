4e5272e3274cbc6551e0270f0e3de72c0e0b509a
jeremygilbertc <jeremy.gilbert.bm@gmail.com>
2022-09-30 14:36:27 +1000

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+/** Main method
+ * 
+ */
+public class Main extends Project {
+
+    public static void main (String[] args) {
+      Main m = new Main();
+      m.run();
+    }
+   
+      
+  }
+    
+  
